4831a427ab09c66bb5cbbdbb86223c045e993dc7
Natalie Halawani <hala1224@gmail.com>
2019-02-22 02:33:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,6 +78,15 @@ class Student
   
   
   def self.all_students_in_grade_9
+    SELECT *
+    FROM students
+    WHERE  grade = 9
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end.second
+    
     
   end  
 end

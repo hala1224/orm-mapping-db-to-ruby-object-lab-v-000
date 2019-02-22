99344a22dd7f12d017bc930c788b4c139c06d610
Natalie Halawani <hala1224@gmail.com>
2019-02-22 02:50:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -128,4 +128,21 @@ class Student
        self.new_from_db(row)
      end.first
    end
+   
+   def self.first_X_students_in_grade_10(num)
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+     DB[:conn].execute(sql, num).map do |row|
+       self.new_from_db(row)
+     end
+   end
+   
+   
+   
+   
 end

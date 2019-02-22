011d5532517c5d270e0af960fa325e73e98568aa
Natalie Halawani <hala1224@gmail.com>
2019-02-22 02:31:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,11 +69,14 @@ class Student
     FROM students
     SQL
 
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
     
   end
+  
+  
+  
   def self.all_students_in_grade_9
     
   end  
